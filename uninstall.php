ee33027306a97d410a3ae508e38c2ef23627e284
Larry <tradesouthwest@outlook.com>
2019-11-19 13:42:42 -0700

    using array

--- uninstall.php
+++ uninstall.php
@@ -5,15 +5,21 @@
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
-global $wpdb;
+
+if ( is_plugin_active( 'unitizr-plus/unitizr-plus.php' ) ) { return; }
 // Delete options.
-$wpdb->query( "DELETE FROM $wpdb->options WHERE option_name LIKE 'unitizr\_%';" );
-$unitizr_options = array(
+global $wpdb;
+$options = array(
 	'unitizr_cstitle_field',
 	'unitizr_csdescription_field',
+	'unitizr_wndproduct_field',
 	'unitizr_wndinwidth_field',
-	'unitizr_wndtaxbase_field'
+	'unitizr_wndtaxbase_field',
+	'unitizr_begin_date',
+	'unitizr_wndinppd_field',
+	'unitizr_wndnada_field',
+	'unitizr_wndmatch_field'
 );
-foreach( $unitizr_options as $option ) {
+foreach( $options as $option ) {
 	delete_option( $option );
 }
